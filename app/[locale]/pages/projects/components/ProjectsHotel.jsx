1b5a8d418146da11df30d0d2e164da96497614f1
EceOwl20 <ecebayramer20@gmail.com>
2024-06-27 12:06:43 +0300

    photos changed

--- app/[locale]/pages/projects/components/ProjectsHotel.jsx
+++ app/[locale]/pages/projects/components/ProjectsHotel.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React from "react";
 import hotelImage from "@/public/assets/images/hotel.jpg";
 
 const Projects3 = () => {
   return (
-    <div 
+    <div
       className="relative h-screen bg-cover bg-center w-full flex justify-start items-end" //mt-24px
-      style={{ backgroundImage: `url(${hotelImage.src})` }}>
-      <div className="relative flex flex-col justify-center items-center w-[570px] h-[400px] p-2 mt-10 text-white ml-10">
-      <div className="bg-black/40 absolute inset-0 z-[1]"></div>
-        <div className="text-center z-10 ">
-        <p className="text-lg lg:text-2xl xl:text-3xl font-semibold mb-8">Otel Otomasyon Sistemleri</p>
-          <text className="text-xs lg:text-base 2xl:text-lg z-99">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
-             type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
-             typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      style={{ backgroundImage: `url(${hotelImage.src})` }}
+    >
+      <div className="relative flex flex-col justify-center items-center w-[570px] p-2 m-10 text-white ">
+        <div className="bg-black/40 absolute inset-0 z-[1]"></div>
+        <div className="flex flex-col text-center gap-[10px] z-10 ">
+          <p className="text-lg lg:text-xl xl:text-2xl font-semibold ">
+          1997 yılından beri yapmış olduğumuz Otel Elektrik, Elektronik ve
+          Otomasyon projeleri; 
+          </p>
+          <span className="text-xl lg:text-3xl xl:text-4xl font-semibold">REFERANSLARIMIZ</span>
+          <text className="text-xs lg:text-base 2xl:text-lg z-99">
+            Hotel Letoon – Fethiye – Mugla
+            – Turkey Kuşadası International Golf Club –Kuşadası-Aydın- Turkey
+            Ramada Resort & Golf Club Kuşadası –Aydın- Turkey Ramada Hotels &
+            Suites Kuşadası –Aydın- Turkey Liberty Fabay Hotel - Fethiye- Mugla-
+            Turkey Liberty Hotels Lykia – Fethiye –Mugla- Turkey Liberty Hotels
+            Lara – Antalya - Turkey Matild Palace a Luxury Collection Hotel-
+            Budapest - Hungary Paradise Garden Hotel- Fethiye- Mugla- Turkey
+            Golf-Sea Valley Bungalows Fethiye- Mugla- Turkey Sundia Fethiye -
+            Ölüdeniz- Fethiye – Mugla- Turkey TTHOTELS Palace And Tropical
+            Sarıgerme Dalaman- Mugla- Turkey
           </text>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects3
+export default Projects3;
--- app/[locale]/pages/projects/components/ProjectsSolar.jsx
+++ app/[locale]/pages/projects/components/ProjectsSolar.jsx
@@ -1,26 +1,30 @@
-import React from 'react'
+import React from "react";
 import solarpower from "@/public/assets/images/solarpower.jpeg";
 
 const Projects2 = () => {
   return (
-    <div 
+    <div
       className="relative h-screen bg-cover bg-center w-full flex justify-end  items-end" //mt-24px
-      style={{ backgroundImage: `url(${solarpower.src})` }}>
-      <div className="relative flex flex-col justify-center items-center w-[570px] h-[400px] p-2 mt-10 text-white mr-10">
-      <div className="bg-black/30 absolute inset-0 z-[1]"></div>
-        <div className="text-center z-10 ">
-        <p className="text-lg lg:text-2xl xl:text-3xl font-semibold mb-8">Güneş Enerji Santralleri</p>
-          <text className="text-xs lg:text-base 2xl:text-lg z-99">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
-             type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
-             typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-              Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      style={{ backgroundImage: `url(${solarpower.src})` }}
+    >
+      <div className="relative flex flex-col justify-center items-center w-full lg:w-1/3 p-2 m-10 text-white">
+        <div className="bg-black/30 absolute inset-0 z-[1]"></div>
+        <div className="flex flex-col text-center z-10 ">
+          <p className="text-lg lg:text-2xl xl:text-3xl font-semibold mb-8">
+            Güneş Enerji Santralleri
+          </p>
+          <text className="text-xs lg:text-base 2xl:text-lg z-99">
+            Çatı Tipi GES Arazi Tipi GES GES projesinin üretim
+            verimliliğini ve ürün dayanıklılığını garanti ediyoruz. On Power
+            Enerji Grubu olarak kurulumunu yaptığımız Güneş Enerji Santralleri,
+            doğal enerji üretiminde dışa bağımlılığın sona ermesi için güçlü
+            adımlar atmaktadır. Doğaya saygı anlayışıyla önemli bir katkı
+            sağlıyoruz.
           </text>
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects2
+export default Projects2;
